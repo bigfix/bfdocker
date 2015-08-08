9b107bda93670a42ae8ea929539ed5636831b215
Garret Taylor <garret.taylor@ie.ibm.com>
2015-08-08 09:35:26 +0100

    fix for #12 add Vagrant CentOS7 box
    Added a basic vagrant box that provisions with docker and the latest
    CentOS 7 image

--- scripts/vagrant-provision.sh
+++ scripts/vagrant-provision.sh
@@ -10,10 +10,10 @@ setenforce 1
 # Add host entry for sandbox.bigfix.com
 echo 127.0.0.1 sandbox.bigfix.com >> /etc/hosts
 
-# apply OS updates
+# Apply updates, required for latest docker
 yum -y update
 
-# run docker install script
+# Run docker install script
 # see https://docs.docker.com/docker/installation/centos/
 curl -sSL https://get.docker.com/ | sh
 
@@ -21,16 +21,9 @@ curl -sSL https://get.docker.com/ | sh
 echo 'SOURCE=/vagrant' >> /home/vagrant/Makefile
 echo 'include /vagrant/Makefile' >> /home/vagrant/Makefile
 
-# Start docker now
+# Start docker
 systemctl enable docker
 systemctl start docker
 
-# pull official centos base image from docker hub
-#docker pull centos:7
-
-# on box restart vboxadd fails to start workaround by updating the tools
-# https://gist.github.com/cedricpim/5986a839ff40e6bded86
-# requires gcc and kernel-devel to build the tools
-yum -y install gcc kernel-devel
-mount /vagrant/resources/VBoxGuestAdditions_4.3.28.iso -o loop /mnt
-sh /mnt/VBoxLinuxAdditions.run --nox11
+# Pull official centos7 base image from docker hub
+docker pull centos:7
