8e012442574d66e51bb07ef2068a2abefabfa46e
Garret Taylor <garret.taylor@ie.ibm.com>
2015-10-05 17:06:50 +0100

    fix #25

--- scripts/vagrant-provision-svr.sh
+++ scripts/vagrant-provision-svr.sh
@@ -30,15 +30,11 @@ systemctl daemon-reload
 systemctl start docker
 systemctl enable docker
 
-# login to docker to get access to the db2express-c image
-# redirect output to stop it echoing back through vagrant output
-docker login -e $1 -u $2 -p $3
-
 # build docker image with the evaluation edition of BigFix server (besserver)
 cd /vagrant/besserver
 
 # set BF_ACCEPT to accept licnese so BigFix installation response file is modified by build.sh
-BF_ACCEPT=true BES_VERSION=$4 bash ./build.sh
+BF_ACCEPT=true BES_VERSION=$1 bash ./build.sh
 
 # start a docker container running BigFix server (besserver)
 docker run -d -p 52311:52311 -p 52311:52311/udp \
